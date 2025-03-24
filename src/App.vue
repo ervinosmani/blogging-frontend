9c3d0249b4591c2ed913a9a6a3db0ab3e36c1cf9
ervinosmani3@gmail.com <ervinosmani3@gmail.com>
2025-03-24 14:45:41 +0100

    update Tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-    <h1 class="text-xl text-black">Tailwind po funksionon!</h1>
-</template>
+    <div class="min-h-screen bg-red-500 text-white flex items-center justify-center text-3xl">
+      Nese ky tekst eshte me sfond te kuq, Tailwind po punon drejt.
+    </div>
+  </template>
+  
+  <style>
+  </style>
+  
 
-<style scoped>
-
-</style>
